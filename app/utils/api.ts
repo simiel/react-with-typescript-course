c9d1cbddbd2b1fed72cdd50dc32bda227c741781
Adjei <samensah036@st.ug.edu.gh>
2025-07-19 13:44:34 +0000

    feat: Implement API utility functions for GitHub data fetching and user scoring

---
+++ app/utils/api.ts
@@ -0,0 +1,97 @@
+const id = "YOUR_CLIENT_ID";
+const sec = "YOUR_SECRET_ID";
+const params = `?client_id=${id}&client_secret=${sec}`;
+
+interface User {
+  id: string;
+  followers: number;
+}
+
+function getErrorMsg(message: string, username: string) {
+  if (message === "Not Found") {
+    return `${username} doesn't exist`;
+  }
+
+  return message;
+}
+
+function getProfile(username: string): Promise<User> {
+  return fetch(`https://api.github.com/users/${username}${params}`)
+    .then((res) => res.json())
+    .then((profile) => {
+      if (profile.message) {
+        throw new Error(getErrorMsg(profile.message, username));
+      }
+
+      return profile;
+    });
+}
+
+function getRepos(username: string): Promise<Repo[]> {
+  return fetch(
+    `https://api.github.com/users/${username}/repos${params}&per_page=100`
+  )
+    .then((res) => res.json())
+    .then((repos) => {
+      if (repos.message) {
+        throw new Error(getErrorMsg(repos.message, username));
+      }
+
+      return repos;
+    });
+}
+
+interface Repo {
+  stargazers_count: number;
+}
+
+function getStarCount(repos: Repo[]) {
+  return repos.reduce(
+    (count, { stargazers_count }) => count + stargazers_count,
+    0
+  );
+}
+
+function calculateScore(followers: number, repos: Repo[]) {
+  return followers * 3 + getStarCount(repos);
+}
+
+function getUserData(player: string): Promise<Player> {
+  return Promise.all([getProfile(player), getRepos(player)]).then(
+    ([profile, repos]) => ({
+      profile,
+      score: calculateScore(profile.followers, repos),
+    })
+  );
+}
+
+interface Player {
+  profile: unknown;
+  score: number;
+}
+
+function sortPlayers(players: Player[]) {
+  return players.sort((a, b) => b.score - a.score);
+}
+
+export function battle(players: [string, string]) {
+  return Promise.all([getUserData(players[0]), getUserData(players[1])]).then(
+    (results) => sortPlayers(results)
+  );
+}
+
+export function fetchPopularRepos(language: string) {
+  const endpoint = window.encodeURI(
+    `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
+  );
+
+  return fetch(endpoint)
+    .then((res) => res.json())
+    .then((data) => {
+      if (!data.items) {
+        throw new Error(data.message);
+      }
+
+      return data.items as Repo[];
+    });
+}
